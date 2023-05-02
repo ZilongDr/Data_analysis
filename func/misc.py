8464a5be941f8dac310d9f16140a0832f52ce3f6
ZilongDr <107624024+ZilongDr@users.noreply.github.com>
2023-05-03 00:37:42 +0200

    add a convert to numpy function

--- func/misc.py
+++ func/misc.py
@@ -20,3 +20,16 @@ def find_array_index(data, element):
 def norm_array(data):
     norm_value=np.amax(data)
     return data/norm_value
+
+# Convert from DataFrame to np, remove nan
+def convert_to_np(data):
+    """" Convert DataFrame to np array
+
+    Args:
+        data (DataFrame):Pandas dataframe
+
+    Returns:
+        numpy array: numpy array with no nan
+    """
+    data=np.array(data.dropna())
+    return data
